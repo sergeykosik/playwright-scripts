21ea2988861963016b55707763b3d6d12b4f087d
Sergey Kosik <sergey.kosik@ocrex.com>
2020-08-18 19:18:00 +0100

    Added brochure test; Added connect-xero file; Updated playwright to 1.3;

--- config.js
+++ config.js
@@ -4,5 +4,7 @@ dotenv.config();
 
 module.exports = {
   baseUrl: `${process.env.BASE_URL}`,
-  psw: `${process.env.PSW}`
+  psw: `${process.env.PSW}`,
+  xeroUsr: `${process.env.XERO_USR}`,
+  xeroPsw: `${process.env.XERO_PSW}`
 };
